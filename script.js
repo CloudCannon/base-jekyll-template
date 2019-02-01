960a3a1ddb76a1fc47872029e351b91f9f45ac98
CloudCannon <support@cloudcannon.com>
2019-02-01 17:36:17 +0000

    Updated 24 files via CloudCannon

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ const navMenu = document.querySelector('.nav-menu');
 if(menuToggle === false) {
   navMenu.style.display = 'grid';
   navMenu.style.opacity = 1;
-  navMenu.style.zIndex = 99;
   menuToggle = true;
 } else if (menuToggle === true) {
   navMenu.style.display = 'none';
   navMenu.style.opacity = 0;
-  navMenu.style.zIndex = 1;
   menuToggle = false;
 };
 };
